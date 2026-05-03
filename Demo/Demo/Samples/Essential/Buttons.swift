0572ed387525f3189a89838641a98a9b722596f9
MING <54872601+1998code@users.noreply.github.com>
2026-05-03 14:57:14 +0800

    Update Buttons.swift

--- Demo/Demo/Samples/Essential/Buttons.swift
+++ Demo/Demo/Samples/Essential/Buttons.swift
@@ -12,7 +12,6 @@ struct Buttons: View {
     var body: some View {
         NavigationStack {
             deleteButton
-            
                 #if !os(watchOS)
                 .toolbar {
                     ToolbarItemGroup(placement: .navigationBarLeading) {
